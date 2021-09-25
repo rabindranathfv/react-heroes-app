f221b2679a49a0dff8dec96dee4e25cbdebf3a36
Rabindranath Ferreira <rabindranathucv@gmail.com>
2021-09-25 02:03:03 +0100

    use lazy loading and generate chunks file with dynamic import loadable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import React, { Component } from 'react';
-import Test from './Components/test';
-import Invoices from './Components/Invoices';
+import Loadable from 'react-loadable';
 
+import Test from './Components/test';
+
+const Loader = (c) => Loadable({
+  loading: () => 'Loading using lazy load',
+  loader: c 
+})
+
+const Prueba = Loader(() => import('./Components/test'))
+const Invoices = Loader(() => import('./Components/Invoices'))
 class App extends Component {
   render () {
     return (
