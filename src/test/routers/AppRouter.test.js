d3dbdbafc4c92cb04e9630d9a77bf64bebfd30b9
Rabindranath Ferreira <ferreiralox@gmail.com>
2022-02-17 19:22:44 +0100

    add test test base for private routes

--- src/test/routers/AppRouter.test.js
+++ src/test/routers/AppRouter.test.js
@@ -2,29 +2,48 @@ import { AppRouter } from '../../routers/AppRouter';
 import { mount } from 'enzyme';
 import { AuthContext } from '../../auth/authContext';
 
-const contextVal = {
-    user: {
-        logged: false
-    }
-}
 describe('AppRouter:::', () => {
 
-    let wrapper = mount(
-    <AuthContext.Provider value={contextVal} > 
-        <AppRouter />
-    </AuthContext.Provider>
-    );
+    // beforeEach(() => {
+    //     wrapper = mount(
+    //         <AuthContext.Provider value={contextVal} > 
+    //             <AppRouter />
+    //         </AuthContext.Provider>
+    //         );
+    // });
 
-    beforeEach(() => {
-        wrapper = mount(
+    it('Should show login route if use is not authenticated', () => {
+        const contextVal = {
+            user: {
+                logged: false
+            }
+        }
+
+        const wrapper = mount(
             <AuthContext.Provider value={contextVal} > 
                 <AppRouter />
             </AuthContext.Provider>
-            );
-    });
+        );
 
-    it('Should show login route if use is not authenticated', () => {
         expect(wrapper).toMatchSnapshot();
         expect(wrapper.find('h1').text().trim()).toBe('Login');
+    });
+
+    it('Should show DashboardRoutes route if use is authenticated', () => {
+        const contextVal = {
+            user: {
+                logged: true,
+                name: 'rabindranath'
+            }
+        }
+
+        const wrapper = mount(
+            <AuthContext.Provider value={contextVal} > 
+                <AppRouter />
+            </AuthContext.Provider>
+        );
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('.navbar').exists()).toBeTruthy();
     });
 });
