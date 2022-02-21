ce9cf42bc40e19308a86e9181d52b338a665e247
Rabindranath Ferreira <ferreiralox@gmail.com>
2022-02-21 19:54:52 +0100

    add based unit test for navbar component

---
+++ src/test/Components/ui/Navbar.test.js
@@ -0,0 +1,54 @@
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
+
+import { AuthContext } from './../../../auth/authContext';
+import { Navbar } from './../../../Components/ui/Navbar';
+
+// !important when you are using jest.mock remenber use mock<FunctionName> for easily mock without break scope variables
+const mockNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockNavigate
+}));
+
+describe('SearchScreen', () => { 
+
+    const contextVal = {
+        user: {
+            logged: true,
+            name: 'rabindranath'
+        },
+        dispatch: () => jest.fn()
+    }
+
+   it('Should render Navbar component with user authenticated', () => {
+       const wrapper = mount(
+        <AuthContext.Provider value={contextVal} > 
+            <MemoryRouter>
+                <Navbar />
+            </MemoryRouter>
+        </AuthContext.Provider>   
+    );
+
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('.text-info').text().trim()).toBe('rabindranath');
+   }); 
+
+   it('Should render Navbar Screen with user authenticated, logout btn and make logout successfuly', () => {
+    const wrapper = mount(
+     <AuthContext.Provider value={contextVal} > 
+         <MemoryRouter>
+             <Navbar />
+         </MemoryRouter>
+     </AuthContext.Provider>   
+    );
+
+    wrapper.find('button').simulate('click');
+
+    expect(wrapper.find('button').text().trim()).toBe('Logout');
+    expect(mockNavigate).toHaveBeenCalled();
+    expect(mockNavigate).toHaveBeenCalledWith('/login', {'replace': true})
+    }); 
+
+});
